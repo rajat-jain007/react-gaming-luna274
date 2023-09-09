12dfe94fdd948952bf905dc39f8417014c0d6b19
rajat-jain007 <rajatbachhawat@gmail.com>
2023-09-09 17:15:26 +0530

    Unnecessary Codes deleted

--- src/components/DropdownItems.jsx
+++ src/components/DropdownItems.jsx
@@ -1,8 +1,5 @@
 import React, { useState } from "react";
 
-// import { DropDownList } from "./Links";
-// import { PiCoinVerticalDuotone } from "react-icons/pi";
-
 import { Dropdown } from "flowbite-react";
 import { DropDownList } from "./Links";
 
