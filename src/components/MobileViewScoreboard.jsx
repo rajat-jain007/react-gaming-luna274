57c57d9d9186598ffda2ba01567cb6521f4c394f
rajat-jain007 <rajatbachhawat@gmail.com>
2023-09-13 12:50:00 +0530

    Optimized MobileViewScoreBoard component

--- src/components/MobileViewScoreboard.jsx
+++ src/components/MobileViewScoreboard.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { cricketScore, soccerScore } from "./Links";
+import { cricketScore, soccerScore, tennisScore } from "./ScoreLinks.jsx";
 
 function MobileViewScoreboard() {
   return (
@@ -103,113 +103,45 @@ function MobileViewScoreboard() {
           <div className="text-white bg-red-700 text-sm font-semibold p-1">
             Tennis
           </div>
-          <div className="text-black bg-slate-100 flex flex-col text-base font-semibold ">
-            A. Cornet v T Zidansek
-            <span className="text-sm font-normal">
-              15 Sep 01:00 | WTA Bari 2023
-            </span>
-          </div>
+          {tennisScore.map((link) => {
+            const { match, placeTime } = link;
+            return (
+              <>
+                <div className="text-black bg-slate-100 flex flex-col text-sm font-semibold w-full">
+                  {match}
+                  <span className="text-sm font-normal truncate">
+                    {placeTime}
+                  </span>
+                </div>
 
-          <div className="flex grid-cols-6 bg-slate-100 gap-1 w-full mb-3  ">
-            <div className="grid-cols-1  bg-sky-400 px-4 mt-2 ">2.60</div>
-            <div className="grid-cols-1  bg-green-300 px-4 mt-2 ">1.60</div>
-
-            <div className="grid-cols-1  bg-sky-400 px-4 mt-2 ">2.55</div>
-            <div className="grid-cols-1  bg-green-300 px-4 mt-2 ">2.75</div>
-
-            <div className="grid-cols-1  bg-sky-400 px-4 mt-2 ">2.60</div>
-            <div className="grid-cols-1  bg-green-300 px-4 mt-2 ">2.80</div>
-          </div>
-
-          <div className="text-black bg-slate-100 flex flex-col text-base font-semibold ">
-            K Siniakova v Zarazua
-            <span className="text-sm font-normal">
-              09 Sep 01:00 | WTA San Diego 2023
-            </span>
-          </div>
-
-          <div className="flex grid-cols-6 bg-slate-100 gap-1 w-full mb-3  ">
-            <div className="grid-cols-1  bg-sky-400 px-4 mt-2 ">2.60</div>
-            <div className="grid-cols-1  bg-green-300 px-4 mt-2 ">1.60</div>
-
-            <div className="grid-cols-1  bg-sky-400 px-4 mt-2 ">2.55</div>
-            <div className="grid-cols-1  bg-green-300 px-4 mt-2 ">2.75</div>
-
-            <div className="grid-cols-1  bg-sky-400 px-4 mt-2 ">2.60</div>
-            <div className="grid-cols-1  bg-green-300 px-4 mt-2 ">2.80</div>
-          </div>
-
-          <div className="text-black bg-slate-100 flex flex-col text-base font-semibold ">
-            A Sasnovich v Er Cayetano
-            <span className="text-sm font-normal">
-              10 Sep 01:00 | WTA San Diego 2023
-            </span>
-          </div>
-
-          <div className="flex grid-cols-6 bg-slate-100 gap-1 w-full mb-3  ">
-            <div className="grid-cols-1  bg-sky-400 px-4 mt-2 ">2.60</div>
-            <div className="grid-cols-1  bg-green-300 px-4 mt-2 ">1.60</div>
-
-            <div className="grid-cols-1  bg-sky-400 px-4 mt-2 ">2.55</div>
-            <div className="grid-cols-1  bg-green-300 px-4 mt-2 ">2.75</div>
-
-            <div className="grid-cols-1  bg-sky-400 px-4 mt-2 ">2.60</div>
-            <div className="grid-cols-1  bg-green-300 px-4 mt-2 ">2.80</div>
-          </div>
-
-          <div className="text-black bg-slate-100 flex flex-col text-base font-semibold ">
-            Gauff v A Sabalenka
-            <span className="text-sm font-normal">
-              10 Sep 01:00 | Women's US Open 2023
-            </span>
-          </div>
-
-          <div className="flex grid-cols-6 bg-slate-100 gap-1 w-full mb-3  ">
-            <div className="grid-cols-1  bg-sky-400 px-4 mt-2 ">2.60</div>
-            <div className="grid-cols-1  bg-green-300 px-4 mt-2 ">1.60</div>
-
-            <div className="grid-cols-1  bg-sky-400 px-4 mt-2 ">2.55</div>
-            <div className="grid-cols-1  bg-green-300 px-4 mt-2 ">2.75</div>
-
-            <div className="grid-cols-1  bg-sky-400 px-4 mt-2 ">2.60</div>
-            <div className="grid-cols-1  bg-green-300 px-4 mt-2 ">2.80</div>
-          </div>
-
-          <div className="text-black bg-slate-100 flex flex-col text-base font-semibold ">
-            C Liu v Aly Ahn
-            <span className="text-sm font-normal">
-              10 Sep 01:00 | WTA San Diego 2023
-            </span>
-          </div>
-
-          <div className="flex grid-cols-6 bg-slate-100 gap-1 w-full mb-3  ">
-            <div className="grid-cols-1  bg-sky-400 px-4 mt-2 ">2.60</div>
-            <div className="grid-cols-1  bg-green-300 px-4 mt-2 ">1.60</div>
-
-            <div className="grid-cols-1  bg-sky-400 px-4 mt-2 ">2.55</div>
-            <div className="grid-cols-1  bg-green-300 px-4 mt-2 ">2.75</div>
-
-            <div className="grid-cols-1  bg-sky-400 px-4 mt-2 ">2.60</div>
-            <div className="grid-cols-1  bg-green-300 px-4 mt-2 ">2.80</div>
-          </div>
-
-          <div className="text-black bg-slate-100 flex flex-col text-base font-semibold ">
-            Camila Osorio v Loeb
-            <span className="text-sm font-normal">
-              10 Sep 01:00 | WTA San Diego 2023
-            </span>
-          </div>
-
-          <div className="flex grid-cols-6 bg-slate-100 gap-1 w-full mb-3  ">
-            <div className="grid-cols-1  bg-sky-400 px-4 mt-2 ">2.60</div>
-            <div className="grid-cols-1  bg-green-300 px-4 mt-2 ">1.60</div>
-
-            <div className="grid-cols-1  bg-sky-400 px-4 mt-2 ">2.55</div>
-            <div className="grid-cols-1  bg-green-300 px-4 mt-2 ">2.75</div>
-
-            <div className="grid-cols-1  bg-sky-400 px-4 mt-2 ">2.60</div>
-            <div className="grid-cols-1  bg-green-300 px-4 mt-2 ">2.80</div>
-          </div>
+                {link.score.map((scoreLink) => {
+                  const { one, two, three, four, five, six } = scoreLink;
+                  return (
+                    <div className="flex grid-cols-6 bg-slate-100 gap-1 mb-3 text-xs font-semibold ">
+                      <div className="grid-cols-1  bg-sky-400 px-4 mt-2 py-1">
+                        {one}
+                      </div>
+                      <div className="grid-cols-1  bg-green-300 px-4 mt-2 py-1">
+                        {two}
+                      </div>
+                      <div className="grid-cols-1  bg-sky-400 px-4 mt-2 py-1">
+                        {three}
+                      </div>
+                      <div className="grid-cols-1  bg-green-300 px-4 mt-2 py-1">
+                        {four}
+                      </div>
+                      <div className="grid-cols-1  bg-sky-400 px-4 mt-2 py-1">
+                        {five}
+                      </div>
+                      <div className="grid-cols-1  bg-green-300 px-4 mt-2 py-1">
+                        {six}
+                      </div>
+                    </div>
+                  );
+                })}
+              </>
+            );
+          })}
         </div>
       </div>
       {326}
---
+++ src/components/ScoreLinks.jsx
@@ -0,0 +1,244 @@
+export const cricketScore = [
+  {
+    match: "South Africa V Australia",
+    placeTime: "10 Sep 01:00 | Women's Caribbean Premier League",
+
+    score: [
+      {
+        one: "2.60",
+        two: "1.60",
+        three: "2.55",
+        four: "2.75",
+        five: "2.60",
+        six: "2.80",
+      },
+    ],
+  },
+
+  {
+    match: "Guyana Warriors W v Trinbago Knight Riders W",
+    placeTime: "10 Sep 01:00 | Women's Caribbean Premier League",
+
+    score: [
+      {
+        one: "2.60",
+        two: "1.60",
+        three: "2.80",
+        four: "2.60",
+        five: "2.78",
+        six: "2.50",
+      },
+    ],
+  },
+
+  {
+    match: "Pakistan v India",
+    placeTime: "10 Sep 01:00 | Asia Cup",
+
+    score: [
+      {
+        one: "2.60",
+        two: "1.60",
+        three: "2.55",
+        four: "2.75",
+        five: "2.60",
+        six: "2.80",
+      },
+    ],
+  },
+
+  {
+    match: "England v New Zealand",
+    placeTime: "10 Sep 01:00 | One Day Internationals",
+
+    score: [
+      {
+        one: "3.60",
+        two: "1.60",
+        three: "5.55",
+        four: "4.75",
+        five: "2.45",
+        six: "3.10",
+      },
+    ],
+  },
+];
+
+export const soccerScore = [
+  {
+    match: "Oman U23 v Jordan U23",
+    placeTime: "10 Sep 01:00 | AFC U23 Championship",
+
+    score: [
+      {
+        one: "2.60",
+        two: "1.60",
+        three: "2.55",
+        four: "2.75",
+        five: "2.60",
+        six: "2.80",
+      },
+    ],
+  },
+
+  {
+    match: "Anderlecht (W) v SK Brann (W)",
+    placeTime: "22 Sep 01:00 | UEFA Women's Champions League",
+
+    score: [
+      {
+        one: "2.60",
+        two: "1.60",
+        three: "2.80",
+        four: "2.60",
+        five: "2.78",
+        six: "2.50",
+      },
+    ],
+  },
+
+  {
+    match: "CA Independiente v Estudiantes",
+    placeTime: "09 Sep 01:00 | Argentinian Cup",
+
+    score: [
+      {
+        one: "2.60",
+        two: "1.60",
+        three: "2.55",
+        four: "2.75",
+        five: "2.60",
+        six: "2.80",
+      },
+    ],
+  },
+
+  {
+    match: "Comoros v Zambia",
+    placeTime: " 10 Sep 01:00 | CAF African Nations Cup Qualifiers",
+
+    score: [
+      {
+        one: "3.60",
+        two: "1.60",
+        three: "5.55",
+        four: "4.75",
+        five: "2.45",
+        six: "3.10",
+      },
+    ],
+  },
+
+  {
+    match: " Oman U23 v Jordan U23",
+    placeTime: "10 Sep 01:00 | AFC U23 Championship",
+
+    score: [
+      {
+        one: "3.60",
+        two: "1.60",
+        three: "5.55",
+        four: "4.75",
+        five: "2.45",
+        six: "3.10",
+      },
+    ],
+  },
+];
+
+export const tennisScore = [
+  {
+    match: " A. Cornet v T Zidansek",
+    placeTime: "15 Sep 01:00 | WTA Bari 2023",
+
+    score: [
+      {
+        one: "2.60",
+        two: "1.60",
+        three: "2.55",
+        four: "2.75",
+        five: "2.60",
+        six: "2.80",
+      },
+    ],
+  },
+
+  {
+    match: "K Siniakova v Zarazua",
+    placeTime: "09 Sep 01:00 | WTA San Diego 2023",
+
+    score: [
+      {
+        one: "2.60",
+        two: "1.60",
+        three: "2.80",
+        four: "2.60",
+        five: "2.78",
+        six: "2.50",
+      },
+    ],
+  },
+
+  {
+    match: "A Sasnovich v Er Cayetano",
+    placeTime: " 10 Sep 01:00 | WTA San Diego 2023",
+
+    score: [
+      {
+        one: "2.60",
+        two: "1.60",
+        three: "2.55",
+        four: "2.75",
+        five: "2.60",
+        six: "2.80",
+      },
+    ],
+  },
+
+  {
+    match: "Gauff v A Sabalenka",
+    placeTime: "10 Sep 01:00 | Women's US Open 2023",
+
+    score: [
+      {
+        one: "3.60",
+        two: "1.60",
+        three: "5.55",
+        four: "4.75",
+        five: "2.45",
+        six: "3.10",
+      },
+    ],
+  },
+
+  {
+    match: "C Liu v Aly Ahn",
+    placeTime: "10 Sep 01:00 | WTA San Diego 2023",
+
+    score: [
+      {
+        one: "3.60",
+        two: "1.60",
+        three: "5.55",
+        four: "4.75",
+        five: "2.45",
+        six: "3.10",
+      },
+    ],
+  },
+  {
+    match: " Camila Osorio v Loeb",
+    placeTime: "10 Sep 01:00 | WTA San Diego 2023",
+
+    score: [
+      {
+        one: "3.60",
+        two: "1.60",
+        three: "5.55",
+        four: "4.75",
+        five: "2.45",
+        six: "3.10",
+      },
+    ],
+  },
+];
