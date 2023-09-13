cce1b61e890ed0d36f6b1d6a5c7465ee05f4d0f2
rajat-jain007 <rajatbachhawat@gmail.com>
2023-09-13 12:16:57 +0530

    Optimized MobileViewScoreBoard component

--- src/components/MobileViewScoreboard.jsx
+++ src/components/MobileViewScoreboard.jsx
@@ -1,83 +1,54 @@
 import React from "react";
+import { cricketScore, soccerScore } from "./Links";
 
 function MobileViewScoreboard() {
   return (
     <div className="">
       <div className="grid  md:hidden bg-white overflow-auto ">
-        {/* Table 1 */}
         <div className="rounded-lg shadow-md">
           <div className="text-white bg-red-700 text-sm font-semibold p-1">
             Cricket
           </div>
-          <div className="text-black bg-slate-100 flex flex-col text-sm font-semibold w-full">
-            South Africa v Australia
-            <span className="text-sm font-normal truncate">
-              10 Sep 01:00 | Women's Caribbean Premier League
-            </span>
-          </div>
+          {/* Table 1 */}
+          {cricketScore.map((link) => {
+            const { match, placeTime } = link;
+            return (
+              <>
+                <div className="text-black bg-slate-100 flex flex-col text-sm font-semibold w-full">
+                  {match}
+                  <span className="text-sm font-normal truncate">
+                    {placeTime}
+                  </span>
+                </div>
 
-          <div className="flex grid-cols-6 bg-slate-100 gap-1 mb-3 text-xs font-semibold">
-            <div className="grid-cols-1  bg-sky-400 px-4 mt-2 ">2.60</div>
-            <div className="grid-cols-1  bg-green-300 px-4 mt-2 ">1.60</div>
-
-            <div className="grid-cols-1  bg-sky-400 px-4 mt-2 ">2.55</div>
-            <div className="grid-cols-1  bg-green-300 px-4 mt-2 ">2.75</div>
-
-            <div className="grid-cols-1  bg-sky-400 px-4 mt-2 ">2.60</div>
-            <div className="grid-cols-1  bg-green-300 px-4 mt-2 ">2.80</div>
-          </div>
-
-          <div className="text-black bg-slate-100 flex flex-col text-base font-semibold ">
-            Guyana Warriors W v Trinbago Knight Riders W
-            <span className="text-sm font-normal">
-              10 Sep 01:00 | Caribbean Premier League League
-            </span>
-          </div>
-
-          <div className="flex grid-cols-6 bg-slate-100 gap-1 w-full mb-3  ">
-            <div className="grid-cols-1  bg-sky-400 px-4 mt-2 ">2.60</div>
-            <div className="grid-cols-1  bg-green-300 px-4 mt-2 ">1.60</div>
-
-            <div className="grid-cols-1  bg-sky-400 px-4 mt-2 ">2.55</div>
-            <div className="grid-cols-1  bg-green-300 px-4 mt-2 ">2.75</div>
-
-            <div className="grid-cols-1  bg-sky-400 px-4 mt-2 ">2.60</div>
-            <div className="grid-cols-1  bg-green-300 px-4 mt-2 ">2.80</div>
-          </div>
-
-          <div className="text-black bg-slate-100 flex flex-col text-base font-semibold ">
-            Pakistan v India
-            <span className="text-sm font-normal">10 Sep 01:00 | Asia Cup</span>
-          </div>
-
-          <div className="flex grid-cols-6 bg-slate-100 gap-1 w-full mb-3  ">
-            <div className="grid-cols-1  bg-sky-400 px-4 mt-2 ">2.60</div>
-            <div className="grid-cols-1  bg-green-300 px-4 mt-2 ">1.60</div>
-
-            <div className="grid-cols-1  bg-sky-400 px-4 mt-2 ">2.55</div>
-            <div className="grid-cols-1  bg-green-300 px-4 mt-2 ">2.75</div>
-
-            <div className="grid-cols-1  bg-sky-400 px-4 mt-2 ">2.60</div>
-            <div className="grid-cols-1  bg-green-300 px-4 mt-2 ">2.80</div>
-          </div>
-
-          <div className="text-black bg-slate-100 flex flex-col text-base font-semibold  ">
-            England v New Zealand
-            <span className="text-sm font-normal">
-              10 Sep 01:00 | One Day Internationals
-            </span>
-          </div>
-
-          <div className="flex grid-cols-6 bg-slate-100 gap-1 w-full mb-3  ">
-            <div className="grid-cols-1  bg-sky-400 px-4 mt-2 ">2.60</div>
-            <div className="grid-cols-1  bg-green-300 px-4 mt-2 ">1.60</div>
-
-            <div className="grid-cols-1  bg-sky-400 px-4 mt-2 ">2.55</div>
-            <div className="grid-cols-1  bg-green-300 px-4 mt-2 ">2.75</div>
-
-            <div className="grid-cols-1  bg-sky-400 px-4 mt-2 ">2.60</div>
-            <div className="grid-cols-1  bg-green-300 px-4 mt-2 ">2.80</div>
-          </div>
+                {link.score.map((scoreLink) => {
+                  const { one, two, three, four, five, six } = scoreLink;
+                  return (
+                    <div className="flex grid-cols-6 bg-slate-100 gap-1 mb-3 text-xs font-semibold ">
+                      <div className="grid-cols-1  bg-sky-400 px-4 mt-2 py-1">
+                        {one}
+                      </div>
+                      <div className="grid-cols-1  bg-green-300 px-4 mt-2 py-1">
+                        {two}
+                      </div>
+                      <div className="grid-cols-1  bg-sky-400 px-4 mt-2 py-1">
+                        {three}
+                      </div>
+                      <div className="grid-cols-1  bg-green-300 px-4 mt-2 py-1">
+                        {four}
+                      </div>
+                      <div className="grid-cols-1  bg-sky-400 px-4 mt-2 py-1">
+                        {five}
+                      </div>
+                      <div className="grid-cols-1  bg-green-300 px-4 mt-2 py-1">
+                        {six}
+                      </div>
+                    </div>
+                  );
+                })}
+              </>
+            );
+          })}
         </div>
 
         {/* Table 2 */}
@@ -85,96 +56,47 @@ function MobileViewScoreboard() {
           <div className="text-white bg-red-700 text-sm font-semibold p-1">
             Soccer
           </div>
-          <div className="text-black bg-slate-100 flex flex-col text-base font-semibold ">
-            Oman U23 v Jordan U23
-            <span className="text-sm font-normal">
-              10 Sep 01:00 | AFC U23 Championship
-            </span>
-          </div>
+          {soccerScore.map((link) => {
+            const { match, placeTime } = link;
+            return (
+              <>
+                <div className="text-black bg-slate-100 flex flex-col text-sm font-semibold w-full">
+                  {match}
+                  <span className="text-sm font-normal truncate">
+                    {placeTime}
+                  </span>
+                </div>
 
-          <div className="flex grid-cols-6 bg-slate-100 gap-1 w-full mb-3  ">
-            <div className="grid-cols-1  bg-sky-400 px-4 mt-2 ">2.60</div>
-            <div className="grid-cols-1  bg-green-300 px-4 mt-2 ">1.60</div>
-
-            <div className="grid-cols-1  bg-sky-400 px-4 mt-2 ">2.55</div>
-            <div className="grid-cols-1  bg-green-300 px-4 mt-2 ">2.75</div>
-
-            <div className="grid-cols-1  bg-sky-400 px-4 mt-2 ">2.60</div>
-            <div className="grid-cols-1  bg-green-300 px-4 mt-2 ">2.80</div>
-          </div>
-
-          <div className="text-black bg-slate-100 flex flex-col text-base font-semibold ">
-            Anderlecht (W) v SK Brann (W)
-            <span className="text-sm font-normal">
-              22 Sep 01:00 | UEFA Women's Champions League
-            </span>
-          </div>
-
-          <div className="flex grid-cols-6 bg-slate-100 gap-1 w-full mb-3  ">
-            <div className="grid-cols-1  bg-sky-400 px-4 mt-2 ">2.60</div>
-            <div className="grid-cols-1  bg-green-300 px-4 mt-2 ">1.60</div>
-
-            <div className="grid-cols-1  bg-sky-400 px-4 mt-2 ">2.55</div>
-            <div className="grid-cols-1  bg-green-300 px-4 mt-2 ">2.75</div>
-
-            <div className="grid-cols-1  bg-sky-400 px-4 mt-2 ">2.60</div>
-            <div className="grid-cols-1  bg-green-300 px-4 mt-2 ">2.80</div>
-          </div>
-
-          <div className="text-black bg-slate-100 flex flex-col text-base font-semibold ">
-            CA Independiente v Estudiantes
-            <span className="text-sm font-normal">
-              09 Sep 01:00 | Argentinian Cup
-            </span>
-          </div>
-
-          <div className="flex grid-cols-6 bg-slate-100 gap-1 w-full mb-3  ">
-            <div className="grid-cols-1  bg-sky-400 px-4 mt-2 ">2.60</div>
-            <div className="grid-cols-1  bg-green-300 px-4 mt-2 ">1.60</div>
-
-            <div className="grid-cols-1  bg-sky-400 px-4 mt-2 ">2.55</div>
-            <div className="grid-cols-1  bg-green-300 px-4 mt-2 ">2.75</div>
-
-            <div className="grid-cols-1  bg-sky-400 px-4 mt-2 ">2.60</div>
-            <div className="grid-cols-1  bg-green-300 px-4 mt-2 ">2.80</div>
-          </div>
-
-          <div className="text-black bg-slate-100 flex flex-col text-base font-semibold ">
-            Comoros v Zambia
-            <span className="text-sm font-normal">
-              10 Sep 01:00 | CAF African Nations Cup Qualifiers
-            </span>
-          </div>
-
-          <div className="flex grid-cols-6 bg-slate-100 gap-1 w-full mb-3  ">
-            <div className="grid-cols-1  bg-sky-400 px-4 mt-2 ">2.60</div>
-            <div className="grid-cols-1  bg-green-300 px-4 mt-2 ">1.60</div>
-
-            <div className="grid-cols-1  bg-sky-400 px-4 mt-2 ">2.55</div>
-            <div className="grid-cols-1  bg-green-300 px-4 mt-2 ">2.75</div>
-
-            <div className="grid-cols-1  bg-sky-400 px-4 mt-2 ">2.60</div>
-            <div className="grid-cols-1  bg-green-300 px-4 mt-2 ">2.80</div>
-          </div>
-
-          <div className="text-black bg-slate-100 flex flex-col text-base font-semibold ">
-            Oman U23 v Jordan U23
-            <span className="text-sm font-normal">
-              10 Sep 01:00 | AFC U23 Championship
-            </span>
-          </div>
-
-          <div className="flex grid-cols-6 bg-slate-100 gap-1 w-full mb-3  ">
-            <div className="grid-cols-1  bg-sky-400 px-4 mt-2 ">2.60</div>
-            <div className="grid-cols-1  bg-green-300 px-4 mt-2 ">1.60</div>
-
-            <div className="grid-cols-1  bg-sky-400 px-4 mt-2 ">2.55</div>
-            <div className="grid-cols-1  bg-green-300 px-4 mt-2 ">2.75</div>
-
-            <div className="grid-cols-1  bg-sky-400 px-4 mt-2 ">2.60</div>
-            <div className="grid-cols-1  bg-green-300 px-4 mt-2 ">2.80</div>
-          </div>
+                {link.score.map((scoreLink) => {
+                  const { one, two, three, four, five, six } = scoreLink;
+                  return (
+                    <div className="flex grid-cols-6 bg-slate-100 gap-1 mb-3 text-xs font-semibold ">
+                      <div className="grid-cols-1  bg-sky-400 px-4 mt-2 py-1">
+                        {one}
+                      </div>
+                      <div className="grid-cols-1  bg-green-300 px-4 mt-2 py-1">
+                        {two}
+                      </div>
+                      <div className="grid-cols-1  bg-sky-400 px-4 mt-2 py-1">
+                        {three}
+                      </div>
+                      <div className="grid-cols-1  bg-green-300 px-4 mt-2 py-1">
+                        {four}
+                      </div>
+                      <div className="grid-cols-1  bg-sky-400 px-4 mt-2 py-1">
+                        {five}
+                      </div>
+                      <div className="grid-cols-1  bg-green-300 px-4 mt-2 py-1">
+                        {six}
+                      </div>
+                    </div>
+                  );
+                })}
+              </>
+            );
+          })}
         </div>
+
         {/* Table 3 */}
 
         <div className="rounded-lg shadow-md">
@@ -290,6 +212,7 @@ function MobileViewScoreboard() {
           </div>
         </div>
       </div>
+      {326}
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}",  'node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}'],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}",
+  ],
 
   plugins: [require("tw-elements/dist/plugin")],
   theme: {
@@ -32,5 +36,5 @@ export default {
       },
     },
   },
-  plugins: [require('flowbite/plugin')],
+  plugins: [require("flowbite/plugin")],
 };
